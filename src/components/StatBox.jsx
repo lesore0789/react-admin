6715b9e30b5e3c27beb972f872e717eb87d0307d
Leslie Orellana <lesore0789@gmail.com>
2024-04-03 19:50:24 -0700

    Finished Dashboard

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -17,6 +17,7 @@ const StatBox = ({ title, subtitle, icon, progress, increase }) => {
         <Box>
           <ProgressCircle progress={progress} />
         </Box>
+      </Box>
         <Box display="flex" justifyContent="space-between">
           <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
             {subtitle}
@@ -25,7 +26,6 @@ const StatBox = ({ title, subtitle, icon, progress, increase }) => {
             {increase}
           </Typography>
         </Box>
-      </Box>
     </Box>
   )
 }
